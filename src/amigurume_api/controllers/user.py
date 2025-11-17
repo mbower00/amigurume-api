6addb6fdc53947624d10f27e45c443338e4c3008
mbower00 <mitchbbower@gmail.com>
2025-11-17 16:45:05 -0500

    test

--- src/amigurume_api/controllers/user.py
+++ src/amigurume_api/controllers/user.py
@@ -148,20 +148,31 @@ class UserController:
     # using code from https://www.youtube.com/watch?v=aX-ayOb_Aho
     def refresh_user(self):
         refresh = request.cookies.get('refresh')
+
         refresh = decode_token(refresh)
+        print(decode_token('sdfds'))
+        print()
+        print()
+        print(decode_token(''))
+        print()
+        print()
+        print(decode_token(None))
+        print()
+        print()
+        print()
         print(refresh)
         return {'message': 'ok'}
-        # username = get_jwt_identity()
-        # with db.session() as session:
-        #     find_user_result = session.execute(
-        #         select(User)
-        #         .where(User.username == username)
-        #     ).first()
-        #     if not find_user_result:
-        #         return {'message': f'No user for token'}, 400
-        #     user = package_result(find_user_result)
-        # access_token = create_access_token(identity=username)
-        # return {'access': access_token, 'username': user['username'], 'clearance': user['clearance']}
+        username = refresh['sub']
+        with db.session() as session:
+            find_user_result = session.execute(
+                select(User)
+                .where(User.username == username)
+            ).first()
+            if not find_user_result:
+                return {'message': f'No user for token'}, 400
+            user = package_result(find_user_result)
+        access_token = create_access_token(identity=username)
+        return {'access': access_token, 'username': user['username'], 'clearance': user['clearance']}
     
     # using code from https://www.youtube.com/watch?v=aX-ayOb_Aho
     def log_out_user(self):
