2fdaa1f83c353fe5b5ed904367bf6dd0832ed55d
mbower00 <mitchbbower@gmail.com>
2025-12-13 09:22:34 -0500

    prod

--- src/amigurume_api/__init__.py
+++ src/amigurume_api/__init__.py
@@ -17,10 +17,10 @@ def create_app():
     app = Flask(__name__)
 
     # using code copied from chatGPT https://chatgpt.com/c/691342fc-371c-832d-8eb1-71fcadf5972f
-    # CORS(app, origins=["https://admin.amigurume.me"], supports_credentials=True)
+    CORS(app, origins=["https://admin.amigurume.me", "https://amigurume.me"], supports_credentials=True)
     # DEVTODO: change this for admin frontend dev
     # using code copied from chatGPT https://chatgpt.com/c/691342fc-371c-832d-8eb1-71fcadf5972f
-    CORS(app, origins=["https://admin.amigurume.me", "http://localhost:5173"], supports_credentials=True)
+    # CORS(app, origins=["https://admin.amigurume.me", "http://localhost:5173"], supports_credentials=True)
 
     # connection string code comes from Google Gemini
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DB_STRING']
